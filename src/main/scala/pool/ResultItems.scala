022f4b95172e7fcf65eccea15a1ce795012096b6
Erik Janssen <erik.w.janssen@gmail.com>
2014-09-12 14:31:00 +0200

    basic functions for Trunk & Branch

--- src/main/scala/pool/ResultItems.scala
+++ src/main/scala/pool/ResultItems.scala
@@ -14,48 +14,103 @@ object ResultItems {
   sealed abstract class Pool[+A] {
     import Pool._
 
-    def rootValue: A //Monoid? needs append method
+    //    def current: A //Monoid? needs append method
+    //    def box: Stream[A]
     def result: Stream[Pool[A]]
 
-    
-        /**
+    def rootValues: Stream[A] = this match {
+      case InCorrect(q, qs, _) => Stream(q).append(qs)
+      case Correct(q, qs, _) => Stream(q).append(qs)
+      case _ => Stream.Empty
+    }
+
+    def addAnswer[B>:A](b: Boolean)(a: B, r: Stream[Pool[B]]) = {
+      def appendAnswer(b: Boolean)(p: Pool[B], a: B) = if (b) appendCorrect(p, a) else appendInCorrect(p, a)
+
+      def appendInCorrect[B>:A](p: Pool[B], a: B): Pool[B] = {
+        p match {
+          case i: InCorrect[A] => InCorrect(i.current, i.box.append(Stream(a)), i.result)
+          case _ => p
+        }
+      }
+      def appendCorrect[B>:A](p: Pool[B], a: B): Pool[B] = {
+        p match {
+          case i: Correct[A] => Correct(i.current, i.box.append(Stream(a)), i.result)
+          case _ => p
+        }
+      }
+      r match {
+        case Stream.Empty => if (b) Stream(cleaf(a)) else Stream(ileaf(a))
+        case _ => r map { p => appendAnswer(b)(p, a)
+
+        }
+      }
+    }
+
+
+    /**
      * Update: updates a Pool with an answers and returns a new Pool
      */
-//        def update[B](b:Boolean)(a: A)(d:(Stream[A],B) => Stream[B]): Pool[B] = {
-    
-//          def updateA[T <: Pool[A]](q: Stream[A], t: Stream[T])(f: (Stream[A], Result[A]) => Stream[T]) = t match {
-//            case Stream.Empty => f(q, Result.Empty.apply)
-//            case _ => f(t.head.appendQ(q), t.head.result)
-//          }
-//    
-//          def updateCorrect(p: Stream[Pool[A]])(q:Stream[A])(b:Boolean): Stream[Pool[A]] =
-//            if (b) updateA(q, p)((q, r) => Stream(correct(q, r))) else p
-//            
-//          def updateInCorrect(p: Stream[Pool[A]])(q:Stream[A])(b:Boolean): Stream[Pool[A]] =
-//            if (!b) updateA(q, p)((q,r) => Stream(incorrect(q, r))) else p
-//    
-//          if (contains(a.head)) this match {
-//            case tc: Correct[A] => correct(dropQ(a.head), Result(updateInCorrect(tc.result.left)(a)(b), updateCorrect(tc.result.right)(a)(b)))
-//            case ti: InCorrect[A] => incorrect(dropQ(a.head), Result(updateInCorrect(ti.result.left)(a)(b), updateCorrect(ti.result.right)(a)(b)))
-//          }
-//          else this
-    
-//        }
-    
-    
+    //    def update[B](b: Boolean)(a: A): Pool[B] = {
+    //      this match {
+    //        //Branches or leafs
+    //        case i:InCorrect[A] => i match {
+    //          case InCorrect(q, Stream.Empty, r) => if(a == q) i.clone(result = r) else InCorrect(q, Stream.Empty, r map {_ update(b)(a)})
+    //        }
+    //        case InCorrect(q, qs, r) =>
+    //        case Correct(q, Stream.Empty, r) => 
+    //        case Correct(q, qs, r) =>
+    //        case _
+    //      }
+
+    //          def updateA[T <: Pool[A]](q: Stream[A], t: Stream[T])(f: (Stream[A], Result[A]) => Stream[T]) = t match {
+    //            case Stream.Empty => f(q, Result.Empty.apply)
+    //            case _ => f(t.head.appendQ(q), t.head.result)
+    //          }
+    //    
+    //          def updateCorrect(p: Stream[Pool[A]])(q:Stream[A])(b:Boolean): Stream[Pool[A]] =
+    //            if (b) updateA(q, p)((q, r) => Stream(correct(q, r))) else p
+    //            
+    //          def updateInCorrect(p: Stream[Pool[A]])(q:Stream[A])(b:Boolean): Stream[Pool[A]] =
+    //            if (!b) updateA(q, p)((q,r) => Stream(incorrect(q, r))) else p
+    //    
+    //          if (contains(a.head)) this match {
+    //            case tc: Correct[A] => correct(dropQ(a.head), Result(updateInCorrect(tc.result.left)(a)(b), updateCorrect(tc.result.right)(a)(b)))
+    //            case ti: InCorrect[A] => incorrect(dropQ(a.head), Result(updateInCorrect(ti.result.left)(a)(b), updateCorrect(ti.result.right)(a)(b)))
+    //          }
+    //          else this
+
+    //    }
+
     //    def merge[B >: A](b:B):B
 
-    /** Maps the elements of the Tree into a Monoid and folds the resulting Tree. */
-    def foldMap[B: Monoid](f: A => B): B =
-      Monoid[B].append(f(rootValue), Foldable[Stream].foldMap[Pool[A], B](result)((_: Pool[A]).foldMap(f)))
-
+    //    /** Maps the elements of the Tree into a Monoid and folds the resulting Tree. */
+    //    def foldMap[B: Monoid](f: A => B): B =
+    //      Monoid[B].append(f(rootValue), Foldable[Stream].foldMap[Pool[A], B](result)((_: Pool[A]).foldMap(f)))
+    //
     def foldRight[B](z: => B)(f: (A, => B) => B): B =
       Foldable[Stream].foldRight(flatten, z)(f)
 
     /** Pre-order traversal. */
-    def flatten = {
-      def squish(pool: Pool[A], xs: Stream[A]): Stream[A] =
-        Stream.cons(pool.rootValue, Foldable[Stream].foldr[Pool[A], Stream[A]](pool.result, xs)(a => b => squish(a, b)))
+    def flatten: Stream[A] = {
+      //      val estream: Stream[A] = Stream.Empty
+      def squish(pool: Pool[A], xs: Stream[A]): Stream[A] = pool match {
+        //Branches or leafs
+        case InCorrect(q, Stream.Empty, r) => Stream.cons(q, Foldable[Stream].foldr[Pool[A], Stream[A]](pool.result, xs)(a => b => squish(a, b)))
+        case InCorrect(q, qs, r) => {
+          val qqs = Stream(q).append(qs)
+          qqs.init.append(Stream.cons(qqs.last, Foldable[Stream].foldr[Pool[A], Stream[A]](pool.result, xs)(a => b => squish(a, b))))
+        }
+
+        case Correct(q, Stream.Empty, r) => Stream.cons(q, Foldable[Stream].foldr[Pool[A], Stream[A]](pool.result, xs)(a => b => squish(a, b)))
+        case Correct(q, qs, r) => {
+          val qqs = Stream(q).append(qs)
+          qqs.init.append(Stream.cons(qqs.last, Foldable[Stream].foldr[Pool[A], Stream[A]](pool.result, xs)(a => b => squish(a, b))))
+        }
+
+        //Trunk
+        case _ => (result map { p => p flatten }).flatten
+      }
       squish(this, Stream.Empty)
     }
 
@@ -64,9 +119,10 @@ object ResultItems {
       val f = (s: Stream[Pool[A]]) => {
         Foldable[Stream].foldMap(s)((_: Pool[A]).result)
       }
-      Stream.iterate(Stream(this))(f) takeWhile (!_.isEmpty) map (_ map (_.rootValue))
+      Stream.iterate(Stream(this))(f) takeWhile (!_.isEmpty) map (_ map (_.rootValues))
     }
 
+    //
     def next = levels.flatten
 
     /**
@@ -77,32 +133,49 @@ object ResultItems {
     def map[B](f: A => B): Pool[B] = {
 
       this match {
-        case InCorrect(q, r) => (q, r) match {
-          case (q, Stream.Empty) => Pool.incorrect(f(q), Stream.Empty)
-          case (q, _) => Pool.incorrect(f(q), r map (_ map f))
+        case ITrunk(r) => ITrunk(r map { _ map f })
+        case CTrunk(r) => CTrunk(r map { _ map f })
+        case InCorrect(q, qs, r) => (q, qs, r) match {
+          case (q, Stream.Empty, Stream.Empty) => Pool.incorrect(f(q), Stream.Empty, Stream.Empty)
+          case (q, qs, Stream.Empty) => Pool.incorrect(f(q), qs map f, Stream.Empty)
+          case (q, qs, r) => Pool.incorrect(f(q), qs map f, r map { _ map f })
         }
-        case Correct(q, r) => (q, r) match {
-          case (q, Stream.Empty) => Pool.correct(f(q), Stream.Empty)
-          case (q, _) => Pool.correct(f(q), r map (_ map f))
+        case Correct(q, qs, r) => (q, qs, r) match {
+          case (q, Stream.Empty, Stream.Empty) => Pool.correct(f(q), Stream.Empty, Stream.Empty)
+          case (q, qs, Stream.Empty) => Pool.correct(f(q), qs map f, Stream.Empty)
+          case (q, qs, r) => Pool.correct(f(q), qs map f, r map { _ map f })
         }
       }
     }
 
-    /**
-     *
-     * flatMap
-     *
-     */
-    def flatMap[B](f: A => Pool[B]): Pool[B] = {
-      this match {
-        case ti: InCorrect[A] =>
-          val r = f(rootValue)
-          Pool.incorrect(r.rootValue, r.result #::: ti.result.map(_.flatMap(f)))
-        case tc: Correct[A] =>
-          val r = f(rootValue)
-          Pool.correct(r.rootValue, r.result #::: tc.result.map(_.flatMap(f)))
-      }
-    }
+    //    /**
+    //     *
+    //     * flatMap
+    //     *
+    //     */
+    //    def flatMap[B](f: A => Pool[B]): Pool[B] = {
+    //      this match {
+    //        case ITrunk(r) => ITrunk(r map { _ flatMap f })
+    //        case CTrunk(r) => CTrunk(r map { _ flatMap f })
+    //        case InCorrect(q, qs, r) => (q, qs, r) match {
+    //          case (q, Stream.Empty, Stream.Empty) => Pool.incorrect(f(q), Stream.Empty, Stream.Empty)
+    //          case (q, qs, Stream.Empty) => Pool.incorrect(f(q), qs map f, Stream.Empty)
+    //          case (q, qs, r) => Pool.incorrect(f(q), qs map f, r map { _ map f })
+    //        }
+    //        case Correct(q, qs, r) => (q, qs, r) match {
+    //          case (q, Stream.Empty, Stream.Empty) => Pool.correct(f(q), Stream.Empty, Stream.Empty)
+    //          case (q, qs, Stream.Empty) => Pool.correct(f(q), qs map f, Stream.Empty)
+    //          case (q, qs, r) => Pool.correct(f(q), qs map f, r map { _ map f })
+    //        }
+    //
+    //        case ti: InCorrect[A] =>
+    //          val r = f(ti.current)
+    //          Pool.incorrect(r.current, r.result #::: ti.result.map(_.flatMap(f)))
+    //        case tc: Correct[A] =>
+    //          val r = f(tc.current)
+    //          Pool.correct(r.current, r.result #::: tc.result.map(_.flatMap(f)))
+    //      }
+    //    }
 
     /**
      *
@@ -113,53 +186,56 @@ object ResultItems {
     def show = this match {
       case x: InCorrect[A] => "<I>"
       case x: Correct[A] => "<C>"
+      case x: ITrunk[A] => "I"
+      case x: CTrunk[A] => "I"
     }
-
   }
 
-  case class InCorrect[A](rootValue: A, result: Stream[Pool[A]]) extends Pool[A]
-  case class Correct[A](rootValue: A, result: Stream[Pool[A]]) extends Pool[A]
-//  case class IBranch[A](rootValue: Nothing, result: Stream[Pool[A]]) extends Pool[A]
-//  case class CBranch[A](rootValue: Nothing, result: Stream[Pool[A]]) extends Pool[A]
-//  case class CLeaf[A](rootValue: A, result: Nothing) extends Pool[A]
-//  case class ILeaf[A](rootValue: A, result: Nothing) extends Pool[A]
+  case class InCorrect[A](current: A, box: Stream[A], result: Stream[Pool[A]]) extends Pool[A]
+  case class Correct[A](current: A, box: Stream[A], result: Stream[Pool[A]]) extends Pool[A]
+  case class ITrunk[A](result: Stream[Pool[A]]) extends Pool[A]
+  case class CTrunk[A](result: Stream[Pool[A]]) extends Pool[A]
 
   object Pool {
 
-    def incorrect[A](qs: A, r: Stream[Pool[A]]): Pool[A] = InCorrect(qs, r)
-    def correct[A](qs: A, r: Stream[Pool[A]]): Pool[A] = Correct(qs, r)
-
+    def incorrect[A, B](q: A, qs: Stream[A], r: Stream[Pool[A]]): Pool[A] = InCorrect(q, qs, r)
+    def correct[A, B](q: A, qs: Stream[A], r: Stream[Pool[A]]): Pool[A] = Correct(q, qs, r)
+    //
+    /** Construct a new Trunk with no values. */
+    def itrunk[A, B](result: => Stream[Pool[A]]): Pool[A] = ITrunk(result)
+    def ctrunk[A](result: => Stream[Pool[A]]): Pool[A] = CTrunk(result)
+    //
     /** Construct a new Tree node. */
-    def inode[A](root: => A, result: => Stream[Pool[A]]): Pool[A] = InCorrect(root, result)
-    def cnode[A](root: => A, result: => Stream[Pool[A]]): Pool[A] = Correct(root, result)
+    def inode[A](root: => A, box: => Stream[A], result: => Stream[Pool[A]]): Pool[A] = InCorrect(root, box, result)
+    def cnode[A](root: => A, box: => Stream[A], result: => Stream[Pool[A]]): Pool[A] = Correct(root, box, result)
 
     /** Construct a tree node with no children. */
-    def ileaf[A](root: => A): Pool[A] = inode(root, Stream.empty)
-    def cleaf[A](root: => A): Pool[A] = cnode(root, Stream.empty)
+    def ileaf[A](root: => A): Pool[A] = inode(root, Stream.empty, Stream.empty)
+    def cleaf[A](root: => A): Pool[A] = cnode(root, Stream.empty, Stream.empty)
+    //
+    //        def apply[A](q: => A, qs: => Stream[A]): Pool[A] = incorrect(q,qs, Stream.Empty)
 
-    def apply[A](qs: => A): Pool[A] = incorrect(qs, Stream.Empty)
-
-    def mergeResult[A](l: Stream[Pool[A]], r: => Stream[Pool[A]])(implicit f: (A, A) => A): Stream[Pool[A]] = {
-      l match {
-        case Stream.Empty => r
-        case _ => l.head match {
-          case i: InCorrect[A] => r match {
-            case Stream.Empty => l
-            case _ => r.head match {
-              case i: InCorrect[A] => Stream(incorrect(f(l.head.rootValue, i.rootValue), mergeResult(l.head.result, i.result)(f))).append(mergeResult(l.tail, r.tail)(f))
-              case c: Correct[A] => (mergeResult(Stream(l.head), r.tail)(f)).append(mergeResult(l.tail, Stream(c))(f))
-            }
-          }
-          case c: Correct[A] => r match {
-            case Stream.Empty => l
-            case _ => r.head match {
-              case i: InCorrect[A] => (mergeResult(l.tail, Stream(i))(f)).append(mergeResult(Stream(l.head), r.tail)(f))
-              case c: Correct[A] => (mergeResult(l.tail, r.tail)(f)).append(Stream(correct(f(l.head.rootValue, c.rootValue), mergeResult(l.head.result, c.result)(f))))
-            }
-          }
-        }
-      }
-    }
+    //    def mergeResult[A](l: Stream[Pool[A]], r: => Stream[Pool[A]])(implicit f: (A, A) => A): Stream[Pool[A]] = {
+    //      l match {
+    //        case Stream.Empty => r
+    //        case _ => l.head match {
+    //          case i: InCorrect[A] => r match {
+    //            case Stream.Empty => l
+    //            case _ => r.head match {
+    //              case i: InCorrect[A] => Stream(incorrect(f(l.head.current, i.current), mergeResult(l.head.result, i.result)(f))).append(mergeResult(l.tail, r.tail)(f))
+    //              case c: Correct[A] => (mergeResult(Stream(l.head), r.tail)(f)).append(mergeResult(l.tail, Stream(c))(f))
+    //            }
+    //          }
+    //          case c: Correct[A] => r match {
+    //            case Stream.Empty => l
+    //            case _ => r.head match {
+    //              case i: InCorrect[A] => (mergeResult(l.tail, Stream(i))(f)).append(mergeResult(Stream(l.head), r.tail)(f))
+    //              case c: Correct[A] => (mergeResult(l.tail, r.tail)(f)).append(Stream(correct(f(l.head.current, c.current), mergeResult(l.head.result, c.result)(f))))
+    //            }
+    //          }
+    //        }
+    //      }
+    //    }
   }
 
   //  sealed abstract class PoolInstances {
@@ -189,16 +265,5 @@ object ResultItems {
   //    }
   //
   //  }
-
-  trait PoolFunctions {
-    /** Construct a new Tree node. */
-    def inode[A](root: => A, result: => Stream[Pool[A]]): Pool[A] = InCorrect(root, result)
-    def cnode[A](root: => A, result: => Stream[Pool[A]]): Pool[A] = Correct(root, result)
-
-    /** Construct a tree node with no children. */
-    def ileaf[A](root: => A): Pool[A] = inode(root, Stream.empty)
-    def cleaf[A](root: => A): Pool[A] = cnode(root, Stream.empty)
-
-  }
 
 }
--- src/test/scala/pool/PoolSpec.scala
+++ src/test/scala/pool/PoolSpec.scala
@@ -14,55 +14,78 @@ object PoolSpec extends Specification {
 
   import Pool._
 
-  //State 0
-  val s0 = ileaf(Stream(1, 2, 3, 4, 5, 6))
+  //First round of answers
   val a1 = Answer(Stream(1), false)
   val a2 = Answer(Stream(2), true)
+  val a3 = Answer(Stream(3), false)
+  val a4 = Answer(Stream(4), true)
+  val a5 = Answer(Stream(5), true)
   val a6 = Answer(Stream(6), true)
+  //Second round of answers
+  val a1_2 = Answer(Stream(1), true)
+  val a2_2 = Answer(Stream(2), true)
+  val a3_2 = Answer(Stream(3), false)
+  val a4_2 = Answer(Stream(4), false)
+  val a5_2 = Answer(Stream(5), true)
+  val a6_2 = Answer(Stream(6), false)
 
-  val s1 = inode(Stream(2, 3, 4, 5, 6), Stream(ileaf(Stream(1))))
-  val s1a = inode(Stream(2, 3, 4, 5, 6), Stream(Correct(Stream(1), Stream.Empty)))
-  val s2b = inode(Stream(3, 4, 5, 6), Stream(ileaf(Stream(1)), cleaf(Stream(2))))
-  val s2c = inode(Stream(3, 4, 5, 6), Stream(ileaf(Stream(1)), cleaf(Stream(2))))
-  val s2 = inode(Stream(6), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5))))
-  val s6 = inode(Stream(6), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5))))
-  val s12 = inode(Stream(16), Stream(ileaf(Stream(11, 14)), cleaf(Stream(12, 13, 15))))
-  val s6ac = inode(Stream(), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5, 6))))
-  val s7 = inode(Stream(), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6))))
-  val s8 = inode(Stream(14), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6))))
-  
-  "mergeResult should work" in {
-    implicit def merge[A](a:Stream[A],b:Stream[A]) = a.append(b)
-    
-    val s1 = Stream(ileaf(Stream(1, 2)), cleaf(Stream(3)))
-    val s2 = Stream(ileaf(Stream(4)))
-    val s3 = Stream(cleaf(Stream(4)))
-    val s4 = Stream(cleaf(Stream(1, 2)), ileaf(Stream(3)))
-    val s02 = Stream(inode(Stream(6), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5)))))
-    val s12 = Stream(inode(Stream(16), Stream(ileaf(Stream(11, 14)), cleaf(Stream(12, 13, 15)))))
-    val s7 = Stream(inode(Stream(), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6)))))
-    val s8 = Stream(ileaf(Stream(14)))
+  //State transitions State0 + Answer1 => State1
+  val s0 = inode(1, Stream(2, 3, 4, 5, 6), Stream.Empty)
+  val s1 = inode(2, Stream(3, 4, 5, 6), Stream(ileaf(1)))
+  val s2 = inode(3, Stream(4, 5, 6), Stream(ileaf(1), cleaf(2)))
+  val s3 = inode(4, Stream(5, 6), Stream(inode(1, Stream(3), Stream.Empty), cleaf(2)))
+  val s4 = inode(5, Stream(6), Stream(inode(1, Stream(3), Stream.Empty), cnode(2, Stream(4), Stream.Empty)))
+  val s5 = inode(6, Stream.Empty, Stream(inode(1, Stream(3), Stream.Empty), cnode(2, Stream(4, 5), Stream.Empty)))
+  val s6 = itrunk(Stream(inode(1, Stream(3), Stream.Empty), cnode(2, Stream(4, 5, 6), Stream.Empty)))
 
-    val m = Pool.mergeResult(s1, s2)
-    val m2 = Pool.mergeResult(s2, s1)
-    val m3 = Pool.mergeResult(s1, s3)
-    val m4 = Pool.mergeResult(s3, s1)
-    val m5 = Pool.mergeResult(s2, s3)
-    val m6 = Pool.mergeResult(s3, s2)
-    val m7 = Pool.mergeResult(s02, s12)
-    val m8 = Pool.mergeResult(s7, s8)
-    val m9 = Pool.mergeResult(s7, s02)
-    
-    m must_== Stream(ileaf(Stream(1, 2, 4)), cleaf(Stream(3)))
-    m2 must_== Stream(ileaf(Stream(4, 1, 2)), cleaf(Stream(3)))
-    m3 must_== Stream(ileaf(Stream(1, 2)), cleaf(Stream(3, 4)))
-    m4 must_== Stream(ileaf(Stream(1, 2)), cleaf(Stream(4, 3)))
-    m5 must_== Stream(ileaf(Stream(4)), cleaf(Stream(4)))
-    m6 must_== Stream(ileaf(Stream(4)), cleaf(Stream(4)))
-    m7 must_== Stream(inode(Stream(6, 16), Stream(ileaf(Stream(1, 4, 11, 14)), cleaf(Stream(2, 3, 5, 12, 13, 15)))))
-    m8 must_== Stream(inode(Stream(14), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6)))))
-    m9 must_== Stream(inode(Stream(6), Stream(inode(Stream(1,4), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6,2,3,5)))))
-  }
+  //State transitions State6 + Answer1_2 => State1_2
+  val s1_2 = itrunk(Stream(inode(3, Stream.Empty, Stream(cleaf(1))), cnode(2, Stream(4, 5, 6), Stream.Empty)))
+  val s2_2 = itrunk(Stream(itrunk(Stream(ileaf(3), cleaf(1))), cnode(2, Stream(4, 5, 6), Stream.Empty)))
+  val s3_2 = itrunk(Stream(itrunk(Stream(ileaf(3), cleaf(1))), cnode(4, Stream(5, 6), Stream(cleaf(2)))))
+  val s4_2 = itrunk(Stream(itrunk(Stream(ileaf(3), cleaf(1))), cnode(5, Stream(6), Stream(ileaf(4), cleaf(2)))))
+  val s5_2 = itrunk(Stream(itrunk(Stream(ileaf(3), cleaf(1))), cnode(6, Stream.Empty, Stream(ileaf(4), cnode(2, Stream(5), Stream.Empty)))))
+  val s6_2 = itrunk(Stream(itrunk(Stream(ileaf(3), cleaf(1))), ctrunk(Stream(inode(4, Stream(6), Stream.Empty), cnode(2, Stream(5), Stream.Empty)))))
+
+  //  val s2c = inode(Stream(3, 4, 5, 6), Stream(ileaf(Stream(1)), cleaf(Stream(2))))
+  //  val s2 = inode(Stream(6), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5))))
+  //  val s6 = inode(Stream(6), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5))))
+  //  val s12 = inode(Stream(16), Stream(ileaf(Stream(11, 14)), cleaf(Stream(12, 13, 15))))
+  //  val s6ac = inode(Stream(), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5, 6))))
+  //  val s7 = inode(Stream(), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6))))
+  //  val s8 = inode(Stream(14), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6))))
+  //  
+  //  "mergeResult should work" in {
+  //    implicit def merge[A](a:Stream[A],b:Stream[A]) = a.append(b)
+  //    
+  //    val s1 = Stream(ileaf(Stream(1, 2)), cleaf(Stream(3)))
+  //    val s2 = Stream(ileaf(Stream(4)))
+  //    val s3 = Stream(cleaf(Stream(4)))
+  //    val s4 = Stream(cleaf(Stream(1, 2)), ileaf(Stream(3)))
+  //    val s02 = Stream(inode(Stream(6), Stream(ileaf(Stream(1, 4)), cleaf(Stream(2, 3, 5)))))
+  //    val s12 = Stream(inode(Stream(16), Stream(ileaf(Stream(11, 14)), cleaf(Stream(12, 13, 15)))))
+  //    val s7 = Stream(inode(Stream(), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6)))))
+  //    val s8 = Stream(ileaf(Stream(14)))
+  //
+  //    val m = Pool.mergeResult(s1, s2)
+  //    val m2 = Pool.mergeResult(s2, s1)
+  //    val m3 = Pool.mergeResult(s1, s3)
+  //    val m4 = Pool.mergeResult(s3, s1)
+  //    val m5 = Pool.mergeResult(s2, s3)
+  //    val m6 = Pool.mergeResult(s3, s2)
+  //    val m7 = Pool.mergeResult(s02, s12)
+  //    val m8 = Pool.mergeResult(s7, s8)
+  //    val m9 = Pool.mergeResult(s7, s02)
+  //    
+  //    m must_== Stream(ileaf(Stream(1, 2, 4)), cleaf(Stream(3)))
+  //    m2 must_== Stream(ileaf(Stream(4, 1, 2)), cleaf(Stream(3)))
+  //    m3 must_== Stream(ileaf(Stream(1, 2)), cleaf(Stream(3, 4)))
+  //    m4 must_== Stream(ileaf(Stream(1, 2)), cleaf(Stream(4, 3)))
+  //    m5 must_== Stream(ileaf(Stream(4)), cleaf(Stream(4)))
+  //    m6 must_== Stream(ileaf(Stream(4)), cleaf(Stream(4)))
+  //    m7 must_== Stream(inode(Stream(6, 16), Stream(ileaf(Stream(1, 4, 11, 14)), cleaf(Stream(2, 3, 5, 12, 13, 15)))))
+  //    m8 must_== Stream(inode(Stream(14), Stream(inode(Stream(), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6)))))
+  //    m9 must_== Stream(inode(Stream(6), Stream(inode(Stream(1,4), Stream(ileaf(Stream(1)), cleaf(Stream(3)))), cleaf(Stream(2, 4, 5, 6,2,3,5)))))
+  //  }
   //
   //  "update should work" in {
   //    sa must_== s1
@@ -71,52 +94,57 @@ object PoolSpec extends Specification {
   //    s0.updateBranch(a1) must_== s1
   //  }
   //
-  //  "levels should work" in {
-  //    s1.levels must_== Stream(2,3,4,5,6,1)  
-  //  }
-  //  
+
   "map should work" in {
-    def add1(x: Stream[Int]): Stream[Int] = x map { _ + 1 }
-    def add0(x: Stream[Int]): Stream[Int] = x map { x => x}
-    def add1String(x: Stream[Int]): Stream[String] = x map { _.toString + "_1" }
-    s0 map add1 must_== ileaf(Stream(2, 3, 4, 5, 6, 7))
-    s6 map add1 must_== inode(Stream(7), Stream(ileaf(Stream(2, 5)), cleaf(Stream(3, 4, 6))))
-    s2 map add1String must_== inode(Stream("6_1"), Stream(ileaf(Stream("1_1", "4_1")), cleaf(Stream("2_1", "3_1", "5_1"))))
-  }
+    def add1(x: Int): Int = x + 1
+    val s0 = inode(1, Stream(2, 3, 4, 5, 6), Stream.Empty)
+    val s1 = inode(2, Stream(3, 4, 5, 6), Stream(ileaf(1)))
+    val s2 = inode(3, Stream(4, 5, 6), Stream(ileaf(1), cleaf(2)))
 
-  "flatMap should work" in {
-    //    def add1(x: Stream[Int]): Pool[Int] = InCorrect(x.head + 1,Stream.Empty) 
-    //    s0 flatMap add1 must_== InCorrect(Stream(2, 3, 4, 5, 6, 7), Stream.Empty)
-    todo
+    s0 map add1 must_== inode(2, Stream(3, 4, 5, 6, 7), Stream.Empty)
+    s1 map add1 must_== inode(3, Stream(4, 5, 6, 7), Stream(ileaf(2)))
+    s2 map add1 must_== inode(4, Stream(5, 6, 7), Stream(ileaf(2), cleaf(3)))
   }
-
+  //
+  //  "flatMap should work" in {
+  //    //    def add1(x: Stream[Int]): Pool[Int] = InCorrect(x.head + 1,Stream.Empty) 
+  //    //    s0 flatMap add1 must_== InCorrect(Stream(2, 3, 4, 5, 6, 7), Stream.Empty)
+  //    todo
+  //  }
+  //
   "flattten should work" in {
-    s2.flatten must_== Stream(Stream(6), Stream(1, 4), Stream(2, 3, 5))
-    s7.flatten.flatten must_== Stream(1, 3, 2, 4, 5, 6)
+    s0.flatten must_== Stream(1, 2, 3, 4, 5, 6)
+    s1.flatten must_== Stream(2, 3, 4, 5, 6, 1)
+    s2.flatten must_== Stream(3, 4, 5, 6, 1, 2)
+    s3.flatten must_== Stream(4, 5, 6, 1, 3, 2)
+    s4.flatten must_== Stream(5, 6, 1, 3, 2, 4)
+    s5.flatten must_== Stream(6, 1, 3, 2, 4, 5)
+    s6.flatten must_== Stream(1, 3, 2, 4, 5, 6)
+    s1_2.flatten must_== Stream(3, 1, 2, 4, 5, 6)
+    s2_2.flatten must_== Stream(3, 1, 2, 4, 5, 6)
   }
+  //
+  //  "foldMap should work" in {
+  //    def add1(x: Stream[Int]): Stream[Int] = x map { _ + 1 }
+  //    s0.foldMap(p => add1(p))
+  //    todo
+  //  }
 
-  "foldMap should work" in {
-    def add1(x: Stream[Int]): Stream[Int] = x map { _ + 1 }
-    s0.foldMap(p => add1(p))
-    todo
-  }
   "foldRight should work" in {
-    val estream:Stream[Int] = Stream.Empty
-	  val fr = s0.foldRight(estream)((a,b) => a.append(b))
-	  val fr7 = s7.foldRight(estream)((a,b) => a.append(b))
-	  fr must_== Stream(1,2,3,4,5,6)
-	  fr7 must_== Stream(1,3,2,4,5,6)
+    val estream: Stream[Int] = Stream.Empty
+    val fr = s0.foldRight(estream)((a, b) => Stream(a).append(b))
+    fr must_== Stream(1, 2, 3, 4, 5, 6)
   }
 
   "levels should work" in {
-    s2.levels must_== Stream(Stream(Stream(6)), Stream(Stream(1, 4), Stream(2, 3, 5)))
-    s7.levels.flatten.flatten must_== Stream(2, 4, 5, 6, 1, 3)
+    s2.levels must_== Stream(Stream(Stream(3, 4, 5, 6)), Stream(Stream(1), Stream(2)))
   }
 
   "next should work" in {
     s0.next.flatten.toList must_== List(1, 2, 3, 4, 5, 6)
-    s7.next.filter(!_.isEmpty).head.toList must_== List(2, 4, 5, 6)
-    s8.next.flatten.toList must_== List(14, 2, 4, 5, 6, 1, 3)
+    s1.next.flatten.toList must_== List(2, 3, 4, 5, 6, 1)
+    s2.next.flatten.toList must_== List(3, 4, 5, 6, 1, 2)
+    s3.next.flatten.toList must_== List(4, 5, 6, 1, 3, 2)
   }
 
   //  "depth should work" in {
